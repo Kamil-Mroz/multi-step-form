ad32d3af0cd693670fe5d0b4ecfe66f3f6880b7f
Kamil Mróz <kamilmp17@gmail.com>
2023-03-24 18:02:11 +0100

    implemented changing steps

--- src/data.ts
+++ src/data.ts
@@ -1,91 +1,87 @@
-export default {
-  time: 'Month',
-  steps: [
-    {
-      id: 1,
-      heading: 'Personal info',
-      text: 'Please provide your name, email address and phone number',
-      name: '',
-      email: '',
-      phone: '',
-    },
-    {
-      id: 2,
-      heading: 'Select your plan',
-      offer: '2 months free',
-      activePlan: null,
-      plans: [
-        {
-          planId: 1,
-          icon: 'icon-arcade.svg',
-          title: 'Arcade',
-          price: {
-            month: 9,
-            year: 90,
-          },
+export default [
+  {
+    id: 0,
+    heading: 'Personal info',
+    text: 'Please provide your name, email address and phone number',
+    name: '',
+    email: '',
+    phone: '',
+  },
+  {
+    id: 1,
+    heading: 'Select your plan',
+    offer: '2 months free',
+    activePlan: null,
+    plans: [
+      {
+        planId: 1,
+        icon: 'icon-arcade.svg',
+        title: 'Arcade',
+        price: {
+          month: 9,
+          year: 90,
         },
-        {
-          planId: 2,
-          icon: 'icon-advanced.svg',
-          title: 'Advanced',
-          price: {
-            month: 12,
-            year: 120,
-          },
+      },
+      {
+        planId: 2,
+        icon: 'icon-advanced.svg',
+        title: 'Advanced',
+        price: {
+          month: 12,
+          year: 120,
         },
-        {
-          planId: 3,
-          icon: 'icon-pro.svg',
-          title: 'Pro',
-          price: {
-            month: 15,
-            year: 150,
-          },
+      },
+      {
+        planId: 3,
+        icon: 'icon-pro.svg',
+        title: 'Pro',
+        price: {
+          month: 15,
+          year: 150,
         },
-      ],
-    },
-    {
-      id: 3,
-      heading: 'Pick add-ons',
-      text: 'Add-ons help enhance your gaming experience',
-      plans: [
-        {
-          planId: 1,
-          title: 'Online service',
-          description: 'Access to multiplayer games',
-          price: {
-            month: 1,
-            year: 10,
-          },
-          checked: false,
+      },
+    ],
+  },
+  {
+    id: 2,
+    heading: 'Pick add-ons',
+    text: 'Add-ons help enhance your gaming experience',
+    plans: [
+      {
+        planId: 1,
+        title: 'Online service',
+        description: 'Access to multiplayer games',
+        price: {
+          month: 1,
+          year: 10,
         },
-        {
-          planId: 2,
-          title: 'Larger storage',
-          description: 'Extra 1TB of cloud save',
-          price: {
-            month: 2,
-            year: 20,
-          },
-          checked: false,
+        checked: false,
+      },
+      {
+        planId: 2,
+        title: 'Larger storage',
+        description: 'Extra 1TB of cloud save',
+        price: {
+          month: 2,
+          year: 20,
         },
-        {
-          planId: 3,
-          title: 'Customizable profile',
-          description: 'Custom theme on your profile',
-          price: {
-            month: 2,
-            year: 20,
-          },
-          checked: false,
+        checked: false,
+      },
+      {
+        planId: 3,
+        title: 'Customizable profile',
+        description: 'Custom theme on your profile',
+        price: {
+          month: 2,
+          year: 20,
         },
-      ],
-    },
-    {
-      id: 4,
-      heading: 'Finishing up',
-      text: 'Double-check everything looks OK before confirming',
-      total: 0,
-    },
-  ],
-}
+        checked: false,
+      },
+    ],
+  },
+  {
+    id: 3,
+    heading: 'Finishing up',
+    text: 'Double-check everything looks OK before confirming',
+  },
+]
--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,67 @@
 import './style.css'
+
+const form = document.querySelector('.form') as HTMLFormElement
+const btnBack = document.querySelector('.btn--back') as HTMLButtonElement
+const btnNext = document.querySelector('.btn--next') as HTMLButtonElement
+const btnConfirm = document.querySelector('.btn--confirm') as HTMLButtonElement
+const formFields = document.querySelectorAll('.form-field')
+
+const thankYouEl = document.querySelector('.thank-you')
+
+type stateType = {
+  period: string
+  step: number
+  plan: {}
+  addOns: []
+  total: number
+  previousStep: number
+}
+
+const state: stateType = {
+  period: 'month',
+  step: 0,
+  plan: {},
+  addOns: [],
+  total: 0,
+  previousStep: 0,
+}
+
+const changeStep = (step: number) => {
+  if (state.step + step >= 0 && state.step + step <= formFields.length - 1) {
+    state.previousStep = state.step
+    state.step += step
+    currentStep()
+  }
+}
+
+const currentStep = () => {
+  formFields[state.previousStep].style.display = 'none'
+  formFields[state.step].style.display = 'flex'
+
+  if (state.step === 0) {
+    btnBack.style.display = 'none'
+  } else {
+    btnBack.style.display = 'block'
+  }
+  if (state.step === formFields.length - 1) {
+    btnNext.style.display = 'none'
+    btnConfirm.style.display = 'block'
+  } else {
+    btnNext.style.display = 'block'
+    btnConfirm.style.display = 'none'
+  }
+}
+const onSubmit = (e: SubmitEvent) => {
+  e.preventDefault()
+
+  form.style.display = 'none'
+  thankYouEl.style.display = 'flex'
+}
+
+const init = () => {
+  currentStep()
+  btnBack?.addEventListener('click', () => changeStep(-1))
+  btnNext?.addEventListener('click', () => changeStep(1))
+  form.addEventListener('submit', onSubmit)
+}
+init()
